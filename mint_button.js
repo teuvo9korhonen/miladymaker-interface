26f5ffae8939310483f217a02e174f03b5bfa5fa
Yuminaut <milkonaut@protonmail.com>
2021-08-24 05:00:42 +0200

    add sale started check

--- mint_button.js
+++ mint_button.js
@@ -145,9 +145,9 @@ const Mint = ({ reserve }) => {
 
   console.log(signedIn);
 
-  // if (!saleStarted) {
-  //   return e("div", { className: "sale-notice" }, "The sale has not started yet.");
-  // }
+  if (!saleStarted) {
+    return e("div", { className: "sale-notice" }, "The sale has not started yet.");
+  }
 
   if (!signedIn) {
     return eSignIn();
