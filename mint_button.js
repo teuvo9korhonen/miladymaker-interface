1d029127d81a8bc9d1d0de251769990ab571a283
Yuminaut <milkonaut@protonmail.com>
2021-08-24 03:26:14 +0200

    make mint thing

--- mint_button.js
+++ mint_button.js
@@ -1,158 +1,127 @@
 "use strict";
+
 const e = React.createElement;
 const useState = React.useState;
 const useEffect = React.useEffect;
 
-const MintButton = () => {
-  const [minted, setMinted] = useState(false);
-
-  if (minted) {
-    return "Mint Test Complete";
-  }
-
-  return e("button", { onClick: () => setMinted(true) }, "Mint Control");
-};
-
-const domContainer = document.querySelector("#mint_button_c");
-ReactDOM.render(e(MintButton), domContainer);
-
-/* CODE HERE TO TRY
-
-export default function Mint() {
-
+const Connect = () => {
   // FOR WALLET
-  const [signedIn, setSignedIn] = useState(false)
-
-  const [walletAddress, setWalletAddress] = useState(null)
+  const [signedIn, setSignedIn] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(null);
 
   // FOR MINTING
-  const [HOW_MANY_MILADY, set_HOW_MANY_MILADY] = useState(1)
+  const [howMany, setHowMany] = useState(0);
+  const [miladyContract, setmiladyContract] = useState(null);
 
-  const [miladyContract, setmiladyContract] = useState(null)
-
-  // INFO FROM SMART Contract
-
-  const [totalSupply, setTotalSupply] = useState(0)
-
-  const [saleStarted, setSaleStarted] = useState(false)
-
-  const [miladyPrice, setMiladyPriceNFT] = useState(0)
-
-  useEffect( async() => { 
-
-    signIn()
-
-  }, [])
+  // INFO FROM SMART CONTRACT
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [saleStarted, setSaleStarted] = useState(false);
+  const [miladyPrice, setMiladyPriceNFT] = useState(0);
 
   async function signIn() {
-    if (typeof window.web3 !== 'undefined') {
+    if (typeof window.web3 !== "undefined") {
       // Use existing gateway
       window.web3 = new Web3(window.ethereum);
-     
     } else {
-      alert("No ETH Interface plugged. Using read-only.");
+      alert("No ETH interface plugged. Using read-only.");
     }
 
-    window.ethereum.enable()
+    window.ethereum
+      .enable()
       .then(function (accounts) {
-        window.web3.eth.net.getNetworkType()
-        // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
-        .then((network) => {console.log(network);if(network != "main"){alert("You are on " + network+ " network. Change network to mainnet or you won't be able to do anything here")} });  
-        let wallet = accounts[0]
-        setWalletAddress(wallet)
-        setSignedIn(true)
-        callContractData(wallet)
-
-  })
-  .catch(function (error) {
-  // Handle error. Likely the user rejected the login
-  console.error(error)
-  })
+        window.web3.eth.net
+          .getNetworkType()
+          // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
+          .then((network) => {
+            console.log(network);
+            if (network != "main") {
+              alert(
+                "You are on " + network + " network. Change network to mainnet or you won't be able to do anything here"
+              );
+            }
+          });
+        let wallet = accounts[0];
+        setWalletAddress(wallet);
+        setSignedIn(true);
+        callContractData(wallet);
+      })
+      .catch(function (error) {
+        // Handle error. Likely the user rejected the login
+        console.error(error);
+      });
   }
-
-//
 
   async function signOut() {
-    setSignedIn(false)
+    setSignedIn(false);
   }
-  
+
   async function callContractData(wallet) {
     // let balance = await web3.eth.getBalance(wallet);
     // setWalletBalance(balance)
-    const miladyContract = new window.web3.eth.Contract(ABI, ADDRESS)
-    setmiladyContract(miladyContract)
+    const miladyContract = new window.web3.eth.Contract(ABI, ADDRESS);
+    setmiladyContract(miladyContract);
 
-    const salebool = await miladyContract.methods.saleIsActive().call() 
+    const salebool = await miladyContract.methods.saleIsActive().call();
     // console.log("saleisActive" , salebool)
-    setSaleStarted(salebool)
+    setSaleStarted(salebool);
 
-    const totalSupply = await miladyContract.methods.totalSupply().call() 
-    setTotalSupply(totalSupply)
+    const totalSupply = await miladyContract.methods.totalSupply().call();
+    setTotalSupply(totalSupply);
 
-    const miladyPrice = await miladyContract.methods.miladyPrice().call() 
-    setMiladyPriceNFT(miladyPrice)
-   
+    const miladyPrice = await miladyContract.methods.miladyPrice().call();
+    setMiladyPriceNFT(miladyPrice);
   }
-  
-  async function mintMiladys(HOW_MANY_MILADY) {
+
+  async function mintMiladys(n) {
     if (miladyContract) {
- 
-      const price = Number(miladyPrice)  * HOW_MANY_MILADY 
+      const price = Number(miladyPrice) * n;
 
-      const gasAmount = await miladyContract.methods.mintMiladyNFT(HOW_MANY_MILADY).estimateGas({from: walletAddress, value: price})
-      console.log("estimated gas",gasAmount)
+      const gasAmount = await miladyContract.methods
+        .mintMiladyNFT(n)
+        .estimateGas({ from: walletAddress, value: price });
+      console.log("estimated gas", gasAmount);
 
-      console.log({from: walletAddress, value: price})
+      console.log({ from: walletAddress, value: price });
 
       miladyContract.methods
-            .mintMiladyNFT(HOW_MANY_MILADY)
-            .send({from: walletAddress, value: price, gas: String(gasAmount)})
-            .on('transactionHash', function(hash){
-              console.log("transactionHash", hash)
-            })
-          
+        .mintMiladyNFT(n)
+        .send({ from: walletAddress, value: price, gas: String(gasAmount) })
+        .on("transactionHash", function (hash) {
+          console.log("transactionHash", hash);
+        });
     } else {
-        console.log("Wallet not online.")
+      console.log("Wallet not online.");
     }
-    
-  };
-
-  
-
-
-
-  return (
-<div>
-    <div id="start">
-      <!-- Connect code here --> 
-      <div className="connect-to-button">
-      {!signedIn ? <button onClick={signIn} className="">Purchase with Metamask</button>
-            :
-            <button onClick={signOut} className="">Connected Wallet: {walletAddress}</button>
-      }
-
-      <input 
-                                      type="number" 
-                                      min="1"
-                                      max="10"
-                                      value={HOW_MANY_MILADY}
-                                      onChange={ e => set_HOW_MANY_MILADY(e.target.value) }
-                                      name="" 
-                                      className="Verdana pl-4 text-4xl  inline bg-grey-lighter  py-2 font-normal rounded text-grey-darkest  font-bold"
-      />
-        {saleStarted ? 
-                <button onClick={() => mintmilady(HOW_MANY_MILADY)} className="#">MINT {HOW_MANY_MILADY} miladys for {(miladyPrice * HOW_MANY_MILADY) / (10 ** 18)} ETH + GAS</button>        
-                  : <button className="mt-4 Verdana text-4xl border-6 bg-blau  text-white hover:text-black p-2 ">System Offline.</button>        
-            
-              }
-      </div>
-    </div>
-</div>
-
-          
-         
-                
-    )
   }
 
-  */
+  const eSignIn = () => e("button", { className: "connect-button", onClick: signIn }, `Connect to MetaMask`);
+  const eSignOut = () => e("button", { className: "connect-button", onClick: signOut }, `Disconnect from MetaMask`);
+
+  const eMint = (n) =>
+    e(
+      "div",
+      { className: "mint-button" },
+      e(
+        "a",
+        { onClick: () => mintMiladys(n) },
+        `Mint ${n} Milady${n > 1 ? "s" : ""} for ${(miladyPrice * n) / 10 ** 18} ETH + gas`
+      )
+    );
+
+  console.log(signedIn);
+
+  if (saleStarted) {
+    return e("div", { className: "sale-notice" }, "The sale has not started yet.");
+  }
+
+  if (!signedIn) {
+    return eSignIn();
+  }
+
+  return e("div", { className: "connect-or-buy" }, eSignOut(), eMint(1), eMint(5), eMint(15), eMint(30));
+};
+
+ReactDOM.render(
+  e(() => Connect()),
+  document.querySelector("#mint")
+);
